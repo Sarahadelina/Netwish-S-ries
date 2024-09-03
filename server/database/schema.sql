cfc9d7f49a10d4abba2f54f58218bc4c5965642a
sarah tuccitto <sarah.tuccitto@hotmail.fr>
2024-09-03 10:51:43 +0200

    nothing

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,25 +1,25 @@
 create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+    id int unsigned primary key auto_increment not null,
+    email varchar(255) not null unique,
+    password varchar(255) not null
 );
 
 create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+    id int unsigned primary key auto_increment not null,
+    title varchar(255) not null,
+    user_id int unsigned not null,
+    foreign key (user_id) references user (id)
 );
 
 create table category (
-  id int unsigned primary key auto_increment not null,
-  name varchar(255) not null
+    id int unsigned primary key auto_increment not null,
+    name varchar(255) not null
 );
 
 create table program (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  synopsis text not null,
-  category_id int unsigned not null,
-  foreign key(category_id) references category(id)
+    id int unsigned primary key auto_increment not null,
+    title varchar(255) not null,
+    synopsis text not null,
+    category_id int unsigned not null,
+    foreign key (category_id) references category (id)
 );
