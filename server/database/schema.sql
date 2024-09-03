78ad8f31cb286faf8d64170f41b3691615192968
sarah tuccitto <sarah.tuccitto@hotmail.fr>
2024-09-03 10:53:17 +0200

    Fixed challenge 05

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,25 +1,28 @@
 create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+    id int unsigned primary key auto_increment not null,
+    email varchar(255) not null unique,
+    password varchar(255) not null
 );
 
 create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+    id int unsigned primary key auto_increment not null,
+    title varchar(255) not null,
+    user_id int unsigned not null,
+    foreign key (user_id) references user (id)
 );
 
 create table category (
-  id int unsigned primary key auto_increment not null,
-  name varchar(255) not null
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL
 );
 
 create table program (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  synopsis text not null,
-  category_id int unsigned not null,
-  foreign key(category_id) references category(id)
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    synopsis TEXT NOT NULL,
+    poster VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    year INT NOT NULL,
+    category_id INT UNSIGNED NOT NULL,
+    Foreign Key (category_id) REFERENCES category (id)
 );
